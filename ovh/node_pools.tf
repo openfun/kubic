bc6c1c5e6d2a042039781558a667066bb594e860
Quentin De Muynck <quentin.demuynck@student-cs.fr>
2023-04-06 17:17:56 +0200

    Add ingress controller

--- ovh/node_pools.tf
+++ ovh/node_pools.tf
@@ -1,8 +1,10 @@
 resource "ovh_cloud_project_kube_nodepool" "pool" {
   service_name   = var.service_name
-  kube_id     = ovh_cloud_project_kube.cluster.id
+  kube_id        = ovh_cloud_project_kube.cluster.id
   name           = "default-pool"
   flavor_name    = "d2-4" # 2 vCPU, 4 GB RAM, à ajuster au besoin
   autoscale      = true
   monthly_billed = false
+  min_nodes      = 1
+  max_nodes      = 10
 }
