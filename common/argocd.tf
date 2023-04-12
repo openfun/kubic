70a0d5d980b9a280d79bb9d03ce27a40e1977da0
Bilel El Yaagoubi <bilel.el-yaagoubi@student-cs.fr>
2023-04-12 14:56:39 +0200

    make it work

--- common/argocd.tf
+++ common/argocd.tf
@@ -1,7 +1,7 @@
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  namespace        = "argocd"
-  create_namespace = true
-}
+# resource "helm_release" "argocd" {
+#   name             = "argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   namespace        = "argocd"
+#   create_namespace = true
+# }
--- common/variables.tf
+++ common/variables.tf
@@ -9,31 +9,7 @@ variable "letsencrypt_email" {
   description = "Email address to use for the clusterIssuer"
 }
 
-variable "s3_access_key" {
-  type        = string
-  description = "The access key to use for S3 API calls"
-  sensitive   = true
-}
-
-variable "s3_secret_key" {
-  type        = string
-  description = "The secret key to use for S3 API calls"
-  sensitive   = true
-}
-
-variable "s3_endpoint" {
-  type        = string
-  description = "The endpoint to use for S3 API calls"
-  default     = "https://s3.gra.io.cloud.ovh.net"
-}
-
-variable "s3_bucket_name" {
-  type        = string
-  description = "The bucket name to use for S3 API calls"
-  default     = "tf-s3-bucket-scaleway"
-}
-
 variable "grafana_hostname" {
-    type        = string
-    description = "The hostname to use for the Grafana ingress"
+  type        = string
+  description = "The hostname to use for the Grafana ingress"
 }
