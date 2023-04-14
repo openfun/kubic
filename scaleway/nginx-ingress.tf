b70148ffdd2150e1c3661ec8e7a128fd97ffd1d5
Bilel El Yaagoubi <bilel.el-yaagoubi@student-cs.fr>
2023-04-14 13:14:25 +0200

    separate ingress controller and providers

---
+++ scaleway/nginx-ingress.tf
@@ -0,0 +1,26 @@
+
+resource "scaleway_lb_ip" "nginx_ip" {
+  zone       = "fr-par-1"
+  project_id = scaleway_k8s_cluster.joy.project_id
+}
+
+output "ingress_ip" {
+  value       = scaleway_lb_ip.nginx_ip.ip_address
+  description = "Adress of the loadbalancer"
+  sensitive   = true
+}
+
+resource "helm_release" "nginx_ingress" {
+  name             = "nginx-ingress"
+  namespace        = "nginx"
+  create_namespace = true
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  values = [templatefile("${path.module}/nginx-values.yml", {
+    zone      = scaleway_lb_ip.nginx_ip.zone
+    ip_adress = scaleway_lb_ip.nginx_ip.ip_address
+  })]
+
+}
