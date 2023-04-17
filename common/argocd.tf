c42c830b411bbd78b410d5099d1c49c9b4bcbec3
Marc-Antoine Godde <marc-antoine.godde@student-cs.fr>
2023-04-17 10:49:56 +0200

    Change var to tf vars

--- common/argocd.tf
+++ common/argocd.tf
@@ -7,11 +7,12 @@ resource "helm_release" "argocd" {
   chart      = "argo/argo-cd"
 
   values = [
-    "${templatefile("argocd-values.yaml",
+    templatefile("argocd-values.yaml",
       {
-        hostName = "argocd.scw-mano.fun-plus.fr",
-        password = "$2a$10$mRtiB9CLUR723Q.8oCM7A.BLSsikNOzag8O1y6s.pgVUTT1W2jSf2"
+        hostName = var.argocd_hostname
+        password = var.argocd_password
       }
-    )}"
+    )
   ]
+
 }
