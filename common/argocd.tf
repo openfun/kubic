8026b7f2b1048c96199b816b5b108575a8cd3741
Marc-Antoine Godde <marc-antoine.godde@student-cs.fr>
2023-04-17 10:24:27 +0200

    Create argocd namespace

--- common/argocd.tf
+++ common/argocd.tf
@@ -1,9 +1,17 @@
 resource "helm_release" "argocd" {
-  name       = "argocd-release"
+  name             = "argocd-release"
+  namespace        = "argocd"
+  create_namespace = true
+
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo/argo-cd"
 
   values = [
-    "${templatefile("argocd-values.yaml", {hostName = "argocd.scw-mano.fun-plus.fr", password = "$2a$10$mRtiB9CLUR723Q.8oCM7A.BLSsikNOzag8O1y6s.pgVUTT1W2jSf2"})}"
+    "${templatefile("argocd-values.yaml",
+      {
+        hostName = "argocd.scw-mano.fun-plus.fr",
+        password = "$2a$10$mRtiB9CLUR723Q.8oCM7A.BLSsikNOzag8O1y6s.pgVUTT1W2jSf2"
+      }
+    )}"
   ]
 }
