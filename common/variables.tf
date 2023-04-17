92243b4ca177ff66c9c100fcb832e3d77fc0e60b
Quentin De Muynck <quentin.demuynck@student-cs.fr>
2023-04-17 15:17:28 +0200

    Fix & lint

--- common/variables.tf
+++ common/variables.tf
@@ -28,6 +28,8 @@ variable "grafana_admin_password" {
   type        = string
   description = "The password of the Grafana UI"
   sensitive   = true
+}
+
 variable "vault_server_hostname" {
   type        = string
   description = "The hostname to use for the Vault server ingress"
