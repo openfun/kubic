26b1bd4bce13a7e1eb108491ead5551c4d331c82
Marc-Antoine Godde <marc-antoine.godde@student-cs.fr>
2023-04-17 09:47:40 +0200

    Update argocd symlink

--- common/argocd.tf
+++ common/argocd.tf
@@ -1,7 +1,9 @@
-# resource "helm_release" "argocd" {
-#   name             = "argocd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd"
-#   create_namespace = true
-# }
+resource "helm_release" "argocd" {
+  name       = "argocd-release"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo/argo-cd"
+
+  values = [
+    "${templatefile("argocd-values.yaml", {hostName = "argocd.scw-mano.fun-plus.fr", password = "$2a$10$mRtiB9CLUR723Q.8oCM7A.BLSsikNOzag8O1y6s.pgVUTT1W2jSf2"})}"
+  ]
+}
