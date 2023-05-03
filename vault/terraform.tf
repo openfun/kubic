698c0d0f43d33ad200e540044a4f7d1af8b9cae0
Quentin De Muynck <quentin.demuynck@student-cs.fr>
2023-05-03 11:21:53 +0200

    Configure k8s auth

--- vault/terraform.tf
+++ vault/terraform.tf
@@ -25,3 +25,32 @@ resource "vault_kv_secret_backend_v2" "example" {
   max_versions         = 5
   delete_version_after = 12600
 }
+
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_role" "vault_backend" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "argocd"
+  bound_service_account_names      = ["argocd-repo-server"]
+  bound_service_account_namespaces = ["argocd"]
+  token_ttl                        = 3600
+  token_policies                   = ["argocd"]
+  audience                         = "vault"
+}
+
+resource "vault_policy" "vault_policy" {
+  name = "argocd"
+
+  policy = <<EOT
+path "kv/*" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+resource "vault_kubernetes_auth_backend_config" "vault_backend_config" {
+  backend                = vault_auth_backend.kubernetes.path
+  kubernetes_host        = var.kubernetes_api_url
+}
--- vault/variables.tf
+++ vault/variables.tf
@@ -8,3 +8,8 @@ variable "vault_url" {
     type        = string
     description = "The URL of the Vault server"
 }
+
+variable "kubernetes_api_url" {
+    type        = string
+    description = "The URL of the Kubernetes API server (can be found in the kubeconfig file)"
+}
