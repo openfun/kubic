9da056af5ba74aec561d9966cadbaa6a84f8cc8c
Élie Caratgé <ecaratge@gmail.com>
2023-05-09 10:27:44 +0200

    Working version of velero setup with terraform

--- scaleway/velero.tf
+++ scaleway/velero.tf
@@ -3,18 +3,13 @@ resource "helm_release" "velero" {
   repository = "https://vmware-tanzu.github.io/helm-charts"
   chart      = "velero"
   namespace  = "velero"
-
-  set {
-    name  = "secret-file"
-    value = templatefile("${path.module}/velero-credentials", {
-      velero_s3_access_key_id = var.velero_s3_access_key_id
-      velero_s3_secret_access_key = var.velero_s3_secret_access_key
-    })
-  }
+  create_namespace = true
 
   values = [templatefile("${path.module}/velero-values.yml", {
     velero_s3_bucket_name     = var.velero_s3_bucket_name
     velero_s3_bucket_region   = var.velero_s3_bucket_region
     velero_s3_bucket_endpoint = var.velero_s3_bucket_endpoint
+    velero_s3_access_key_id = var.velero_s3_access_key_id
+    velero_s3_secret_access_key = var.velero_s3_secret_access_key
   })]
 }
