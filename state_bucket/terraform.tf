10e610c99c8ed1475108c543a05e9ded89eef229
Quentin De Muynck <quentin.demuynck@student-cs.fr>
2023-05-12 08:52:31 +0200

    README fix + add missing variable

--- state_bucket/terraform.tf
+++ state_bucket/terraform.tf
@@ -20,12 +20,14 @@ provider "ovh" {
 
 # Configure the AWS Provider
 provider "aws" {
-  region     = var.region
+  region     = var.s3_region
   access_key = ovh_cloud_project_user_s3_credential.s3_admin_cred.access_key_id
   secret_key = ovh_cloud_project_user_s3_credential.s3_admin_cred.secret_access_key
 
+  # OVH implementation has no STS service
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+  # the gra region is unknown to AWS hence skipping is needed.
   skip_region_validation      = true
   endpoints {
     s3 = var.s3_endpoint
