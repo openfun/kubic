6e8507f6d7f80dd9accc34717c4f6510bd366886
Bilel El Yaagoubi <bilel.el-yaagoubi@student-cs.fr>
2023-05-16 13:34:53 +0200

    Add a get kube config script

---
+++ bin/get-kube-config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Retrieve the directory path from the first argument
+directory=$1
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    echo "The directory '$directory' does not exist."
+    exit 1
+fi
+
+DOCKER_USER="$(id -u):$(id -g)" \
+    docker-compose run --rm tf-$directory output -raw kube_config
