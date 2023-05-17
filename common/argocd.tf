8718fe541531c84d3009703ff0677fb708488668
Marc-Antoine Godde <marc-antoine.godde@student-cs.fr>
2023-05-17 12:43:13 +0200

    Fix Vault dependency

--- common/argocd.tf
+++ common/argocd.tf
@@ -10,20 +10,23 @@ resource "helm_release" "argocd" {
   values = [
     templatefile("${path.module}/argocd-values.yaml.tftpl",
       {
-        host_name           = var.argocd_hostname
-        password            = var.argocd_password
-        cluster_issuer_name = var.main_cluster_issuer_name
-        repo_url            = var.argocd_repo_url
-        repo_username       = var.argocd_repo_username
-        repo_password       = var.argocd_repo_password
-        avp_version         = var.argocd_avp_version
-        vault_addr          = var.vault_server_hostname
+        host_name               = var.argocd_hostname
+        password                = var.argocd_password
+        cluster_issuer_name     = var.main_cluster_issuer_name
+        repo_url                = var.argocd_repo_url
+        repo_username           = var.argocd_repo_username
+        repo_password           = var.argocd_repo_password
+        avp_version             = var.argocd_avp_version
+        install_hashicorp_vault = var.install_hashicorp_vault
+        vault_addr              = var.vault_server_hostname
       }
     )
   ]
 }
 
 resource "helm_release" "argocd-apps" {
+  count = var.install_hashicorp_vault ? 1 : 0
+
   name = "argocd-apps"
 
   repository = "https://argoproj.github.io/argo-helm"
