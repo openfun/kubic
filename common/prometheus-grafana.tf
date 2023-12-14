950822f5a011b67e27c741b5201fa9edae73dedb
Bizouard Paul <paul.bizouard@centralesupelec.fr>
2023-12-14 16:01:16 +0100

    🏗️ Add Grafana persistent storage configuration

--- common/prometheus-grafana.tf
+++ common/prometheus-grafana.tf
@@ -10,5 +10,7 @@ resource "helm_release" "kube-prometheus" {
     hostname               = var.grafana_hostname
     issuer                 = var.main_cluster_issuer_name
     grafana_admin_password = var.grafana_admin_password
+    grafana_persistence_enabled = var.grafana_persistence_enabled
+    grafana_persistence_size = var.grafana_persistence_size
   })]
 }
